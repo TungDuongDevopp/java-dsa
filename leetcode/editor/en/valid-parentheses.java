0ca6b518fec66e699063fb76a7a50d8c4c54f022
TungDuong-Dev <devopptungduong@gmail.com>
2026-07-27 19:54:32 +0700

    Solve LC20

---
+++ leetcode/editor/en/valid-parentheses.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch== '[' || ch=='{'){
+                stack.push(ch);
+            }
+            else{
+                if (stack.isEmpty()) return false;
+                char open = stack.pop();
+                if ((ch== ')' && open!= '(') ||
+                        (ch == '}' && open != '{') ||
+                        (ch == ']' && open != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
+
